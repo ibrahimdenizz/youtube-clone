91cc210f39c317941b0e0f57c24416046e5b61d3
ibrahim <dnzibrahim1@gmail.com>
2021-09-12 00:36:17 +0300

    Update home page

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import DefaultLayout from "../layouts/default";
+import "../styles/globals.scss";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <DefaultLayout>
+      <Component {...pageProps} />
+    </DefaultLayout>
+  );
 }
 
-export default MyApp
+export default MyApp;
